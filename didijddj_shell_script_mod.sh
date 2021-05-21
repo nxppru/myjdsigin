04fb89219c0baf2b8e3dfa3b5b4c788923511502
nxppru <nxppru@qq.com>
2021-05-21 05:37:15 +0000

    Create didijddj_shell_script_mod.sh

---
+++ didijddj_shell_script_mod.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+## CUSTOM_SHELL_FILE for https://gitee.com/lxk0301/jd_docker/tree/master/docker
+### 编辑docker-compose.yml文件添加: - CUSTOM_SHELL_FILE=https://raw.githubusercontent.com/passerby-b/JDDJ/main/shell_script_mod.sh
+#### 容器完全启动后执行 docker exec -it jd_scripts /bin/sh -c 'crontab -l' 查看是否成功添加京东到家的定时任务
+
+function jddj(){
+    # clone
+    rm -rf /scripts/jddj
+    git clone https://github.com/passerby-b/JDDJ.git /scripts/jddj
+    # 获取js文件中cron字段设置定时任务
+    for jsname in $(ls /scripts/jddj | grep -E "js$" | tr "\n" " "); do
+        jsnamecron="$(cat /scripts/jddj/$jsname | grep -oE "/?/?cron \".*\"" | cut -d\" -f2)"
+        test -z "$jsnamecron" || echo "$jsnamecron node /scripts/jddj/$jsname >> /scripts/logs/$jsname.log 2>&1" >> /scripts/docker/merged_list_file.sh
+    done
+}
+
+function didi_fruit(){
+    # https://raw.githubusercontent.com/passerby-b/didi_fruit/main/dd_fruit.js
+    wget -qO /scripts/jddj/dd_fruit.js https://raw.githubusercontent.com/passerby-b/didi_fruit/main/dd_fruit.js
+    echo "10 0,8,12,18 * * * node /scripts/jddj/dd_fruit.js >> /scripts/logs/dd_fruit.js.log 2>&1" >> /scripts/docker/merged_list_file.sh
+}
+
+function main(){
+    # 首次运行时拷贝docker目录下文件
+    [[ ! -d /jd_diy ]] && mkdir /jd_diy && cp -rf /scripts/docker/* /jd_d
+    # DIY脚本执行前后信息
+    a_jsnum=$(ls -l /scripts/jddj | grep -oE "^-.*js$" | wc -l)
+    a_jsname=$(ls -l /scripts/jddj | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    jddj
+    didi_fruit
+    b_jsnum=$(ls -l /scripts/jddj | grep -oE "^-.*js$" | wc -l)
+    b_jsname=$(ls -l /scripts/jddj | grep -oE "^-.*js$" | grep -oE "[^ ]*js$")
+    # DIY脚本更新TG通知
+    info_more=$(echo $a_jsname  $b_jsname | tr " " "\n" | sort | uniq -c | grep -oE "1 .*$" | grep -oE "[^ ]*js$" | tr "\n" " ")
+    [[ "$a_jsnum" == "0" || "$a_jsnum" == "$b_jsnum" ]] || curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=DIY脚本更新完成：$a_jsnum $b_jsnum $info_more" >/dev/null
+    # LXK脚本更新TG通知
+    lxktext="$(diff /jd_diy/crontab_list.sh /scripts/docker/crontab_list.sh | grep -E "^[+-]{1}[^+-]+" | grep -oE "node.*\.js" | cut -d/ -f3 | tr "\n" " ")"
+    test -z "$lxktext" || curl -sX POST "https://api.telegram.org/bot$TG_BOT_TOKEN/sendMessage" -d "chat_id=$TG_USER_ID&text=LXK脚本更新完成：$(cat /jd_diy/crontab_list.sh | grep -vE "^#" | wc -l) $(cat /scripts/docker/crontab_list.sh | grep -vE "^#" | wc -l) $lxktext" >/dev/null
+    # 拷贝docker目录下文件供下次更新时对比
+    cp -rf /scripts/docker/* /jd_diy
+}
+
+main
